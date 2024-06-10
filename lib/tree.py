f7a8b41285cf9dd92337e522f98faeaefa09a0b9
Gyzelle <gyznjeri@gmail.com>
2024-06-10 15:22:48 +0300

    python-p3-dsa-tree-traversal

--- lib/tree.py
+++ lib/tree.py
@@ -3,4 +3,15 @@ class Tree:
     self.root = root
 
   def get_element_by_id(self, id):
-    pass
+    # initialize a list with the root node of the tree
+    nodes_to_visit = [self.root]
+
+    while nodes_to_visit:
+      #removes the first node
+      node = nodes_to_visit.pop(0)
+      if node['id'] == id:
+        return node
+      # adds its children to the end of the list if no match is found
+      nodes_to_visit = nodes_to_visit + node['children']
+
+    return None # if the loop completes without finding a matching node, the method returns None
